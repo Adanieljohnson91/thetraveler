523f29e4f77bbfd3492131fbc8801cfd5890846a
Chris Huckins <chris.huckins@tlgcohort.com>
2021-05-04 15:27:12 -0400

    Setup MusicPlayer to be able to adjust volume.  Created basic tests to validate volume methods in the MusicPlayer function,AB#356

--- src/com/escaperooms/music/MusicPlayer.java
+++ src/com/escaperooms/music/MusicPlayer.java
@@ -7,10 +7,21 @@ import javax.sound.sampled.*;
 public class MusicPlayer extends Thread{
     String song;
     Clip clip;
+    float volume = 0.5f;
 
     public MusicPlayer(String song) {
         this.song = song;
     }
+    public MusicPlayer(String song, float volume) {
+        this.song = song;
+        try {
+            setVolume(volume);
+        }
+        catch (IllegalArgumentException iae){
+            volume = 0.5f;
+        }
+    }
+
     public void run() {
         try {
             URL url = MusicPlayer.class.getResource("/resources/" + song);
@@ -19,11 +30,28 @@ public class MusicPlayer extends Thread{
             clip = AudioSystem.getClip();
             clip.open(audioStream);
             clip.start();
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(20f * (float)Math.log10(volume));
+            /*
             while(clip.getMicrosecondLength() != clip.getMicrosecondPosition())
             {
             }
+            */
+
         } catch (Exception e) {
             Thread.currentThread().interrupt();
+        }
+    }
+
+    public void playMusic(){
+        if (clip != null && !clip.isActive()){
+            clip.start();
+        }
+    }
+
+    public void pauseMusic(){
+        if (clip != null && clip.isActive()){
+            clip.stop();
         }
     }
 
@@ -44,4 +72,26 @@ public class MusicPlayer extends Thread{
         return this.clip;
     }
 
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) throws IllegalArgumentException{
+        if (volume < 0.0f || volume > 1.0f){
+            throw new IllegalArgumentException("Volume must be a value of 0.0f - 1.0f inclusive.");
+        }
+        this.volume = volume;
+        if (clip != null && clip.isActive()){
+            FloatControl volumeControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            volumeControl.setValue(20f * (float)Math.log10(volume));
+        }
+    }
+
+    float getClipVol() {
+        if (clip != null) {
+            FloatControl volume = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            return (float) Math.pow(10f, volume.getValue() / 20f);
+        }
+        return -1.0f;
+    }
 }
