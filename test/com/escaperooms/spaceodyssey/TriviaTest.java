e786b53df8c600eab97320d8699ba9471445d913
Amin Elhami <Amin747@gmail.com>
2021-05-13 12:41:55 -0700

    Added test classes, AB#512

---
+++ test/com/escaperooms/spaceodyssey/TriviaTest.java
@@ -0,0 +1,32 @@
+package com.escaperooms.spaceodyssey;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+public class TriviaTest {
+    private TriviaV2 trivia;
+    private List<String> answers;
+    private String correctAnswer;
+    private String question;
+
+    @Before
+    public void setUp(){
+        answers = List.of("white", "blue", "red", "black");
+        correctAnswer = "blue";
+        question = "What color is the sky?";
+        trivia = new TriviaV2(question, answers, correctAnswer);
+    }
+
+    @Test
+    public void getAnswersTest(){
+        assertEquals(List.of("white", "blue", "red", "black"), trivia.getAnswers());
+    }
+
+    @Test
+    public void getCurrentAnswerTest(){
+        assertEquals("blue", trivia.getCorrectAnswer());
+    }
+}
